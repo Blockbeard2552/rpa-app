ac4fe6639923112dbf61d13503004b5d49a68d9e
Blockbeard2552 <joey.stephens524@gmail.com>
2025-05-07 10:59:53 -0400

    hero section.

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,24 @@
+<section class="default-margin hero">
+	<div class="hero-text">
+		<h1>Hero Section</h1>
+		<h3>Strapline.</h3>
+		<h4 class="mt-l">Additional info.</h4>
+		<h4 class="mb-s">what is thie CTA</h4>
+		<button>Sign Up</button>
+	</div>
+</section>
+
+<style>
+	.hero {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-top: 100px;
+		margin-bottom: 80px;
+	}
+	.hero-text {
+		display: flex;
+		flex-direction: column;
+		align-items: start;
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$components: 'src/lib/components',
+			$stores: 'src/lib/stores',
+			$utils: 'src/lib/utils',
+			$assets: 'src/assets',
+			$routes: 'src/routes'
+		}
+	}
 };
 
 export default config;
