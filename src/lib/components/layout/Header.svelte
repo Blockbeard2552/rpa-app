aa9b494310eaa9897540b1e6f7314ddc7b511a2e
Blockbeard2552 <joey.stephens524@gmail.com>
2025-05-08 16:42:09 -0400

    added user state.

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import icon from '$assets/icon.svg';
 	import Button from '$components/Button.svelte';
+	import { getUserState } from '$components/state/user-state.svelte';
+
+	let userContext = getUserState();
+	let { user } = $derived(userContext);
 </script>
 
 <header>
@@ -8,14 +12,23 @@
 		<img src={icon} alt="Go to home" class="logo" />
 	</a>
 	<nav>
-		<ul>
-			<li>
-				<Button isMenu={true} href="/register">Create Account</Button>
-			</li>
-			<li>
-				<Button isMenu={true} isSecondary={true} href="/login">Login</Button>
-			</li>
-		</ul>
+		{#if !user}
+			<ul>
+				<li>
+					<Button isMenu={true} href="/register">Create Account</Button>
+				</li>
+				<li>
+					<Button isMenu={true} isSecondary={true} href="/login">Login</Button>
+				</li>
+			</ul>
+		{:else}
+			<ul>
+				<li>{user.email}</li>
+				<li>
+					<Button isMenu={true} onclick={() => userContext.logout()}>Logout</Button>
+				</li>
+			</ul>
+		{/if}
 	</nav>
 </header>
 
@@ -28,6 +41,7 @@
 	}
 	ul {
 		display: flex;
+		align-items: center;
 		gap: 1rem;
 	}
 	.logo {
